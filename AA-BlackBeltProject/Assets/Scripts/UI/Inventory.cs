1753763eae7c86208791c311452a63fa2a5a0efe
Aroosh <aroosh.manha@gmail.com>
2023-04-13 17:55:26 -0400

    change image on slot when obj is picked up

--- AA-BlackBeltProject/Assets/Scripts/UI/Inventory.cs
+++ AA-BlackBeltProject/Assets/Scripts/UI/Inventory.cs
@@ -8,10 +8,16 @@ public class Inventory : MonoBehaviour
     //public GameObject[] slots;
     public List<GameObject> slots = new List<GameObject>();
     int slotNumber;
+    public GameObject b1;
+    public GameObject b2;
+    public GameObject b3;
+    public GameObject b4;
+    public GameObject b5;
 
     void Start()
     {
         inventory = this;
+        GameObject[] bordersArr = {b1, b2, b3, b4, b5};
     }
 
     void Update()
@@ -19,26 +25,51 @@ public class Inventory : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             slotNumber = 0;
+            b1.SetActive(true);
+            b2.SetActive(false);
+            b3.SetActive(false);
+            b4.SetActive(false);
+            b5.SetActive(false);
         }
         
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             slotNumber = 1;
+            b2.SetActive(true);
+            b1.SetActive(false);
+            b3.SetActive(false);
+            b4.SetActive(false);
+            b5.SetActive(false);
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
             slotNumber = 2;
+            b3.SetActive(true);
+            b2.SetActive(false);
+            b1.SetActive(false);
+            b4.SetActive(false);
+            b5.SetActive(false);
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha4))
         {
             slotNumber = 3;
+            b4.SetActive(true);
+            b2.SetActive(false);
+            b3.SetActive(false);
+            b1.SetActive(false);
+            b5.SetActive(false);
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha5))
         {
             slotNumber = 4;
+            b5.SetActive(true);
+            b2.SetActive(false);
+            b3.SetActive(false);
+            b4.SetActive(false);
+            b1.SetActive(false);
         }
     }
     public void AddInventory(GameObject item)
