5a1051d1a0d91867fe15f83884aed43f99fcdd03
Aroosh <aroosh.manha@gmail.com>
2023-04-26 17:55:21 -0400

    make canvases or planels or whatever

--- AA-BlackBeltProject/Assets/Scripts/UI/SideButtons.cs
+++ AA-BlackBeltProject/Assets/Scripts/UI/SideButtons.cs
@@ -8,7 +8,7 @@ public class SideButtons : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Menu.SetActive(false);
     }
 
     // Update is called once per frame
