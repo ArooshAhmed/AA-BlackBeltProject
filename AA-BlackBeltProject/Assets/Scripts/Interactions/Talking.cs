4ef18797f5678fa5399384a8c1a8b28e941130ac
Aroosh <aroosh.manha@gmail.com>
2023-05-03 18:01:07 -0400

    make the next button work

--- AA-BlackBeltProject/Assets/Scripts/Interactions/Talking.cs
+++ AA-BlackBeltProject/Assets/Scripts/Interactions/Talking.cs
@@ -1,19 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Talking : NPC, iInteractable
 {
-
+    public static GameObject dialogueBox;
+    public List<string> Dialogue = new List<string>();
+    public int DialogueNumber;
+    public static GameObject dialogueText;
     public void interact()
     {
+        dialogueBox.SetActive(true);
+        dialogueText.GetComponent<TextMeshProUGUI>().text = Dialogue[DialogueNumber];
+        DialogueNumber += 1;
+        if (DialogueNumber == Dialogue.Count) 
+        {
+            dialogueBox.SetActive(false);
+        }
+      
         //show a dialogue
         //There are arrows to move on to the next dialogue
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        dialogueText = GameObject.Find("Canvas/DialogueBox/Dialogue");
+        dialogueBox = GameObject.Find("Canvas/DialogueBox");
+        dialogueBox.SetActive(false);
     }
 
     // Update is called once per frame
